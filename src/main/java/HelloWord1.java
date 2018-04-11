ad10ebccf8d4ec98d54634269729c97884097b50
Galla <Surekha.Galla@allstate.com>
2018-04-11 13:13:26 +0530

    Second commit

--- src/main/java/HelloWord1.java
+++ src/main/java/HelloWord1.java
@@ -4,6 +4,8 @@ public class HelloWord1 {
 	public static void main(String[] args) {
 		System.out.print("Hello Java1");
 		System.out.print("Hello Java1");
+		System.out.print("Hello Java1");
+		System.out.print("Hello Java1");
 	}
 
 }
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,6 +3,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.print("Hello Java");
+		System.out.print("Hello Java");
+		System.out.print("Hello Java");
+		System.out.print("Hello Java");
 	}
 
 }
